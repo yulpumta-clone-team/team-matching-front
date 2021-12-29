ad5c98e00e907cb5715aebc51cae87e0ffdd2a3b
YUNHO <kimyouknow@naver.com>
2021-12-29 19:29:29 +0900

    Setting form validation & oauth

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '@layouts/App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
--- src/pages/LogIn/index.tsx
+++ src/pages/LogIn/index.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import { fetchLogin } from '@api/userAction';
+import Loading from '@components/Loading';
+import { UserValidation } from '@typings/userValidation';
+import React, { useCallback } from 'react';
+import { useForm } from 'react-hook-form';
+import { useQuery } from 'react-query';
+import { useNavigate, Link } from 'react-router-dom';
 
 function Login() {
-  return <div>login</div>;
+  const navigate = useNavigate();
+  // const { isLoading, error, data, isFetching } = useQuery('userData', () =>
+  //   fetch('/api/users').then((res) => res.json()),
+  // );
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+    // watch,
+  } = useForm<UserValidation>({
+    defaultValues: {},
+  });
+  const onValid = (data: UserValidation) => {
+    setError('extraError', { message: 'Server offLine.' });
+  };
+  const oauthGithub = useCallback(() => {
+    navigate('/oauth2/authorization/github');
+  }, []);
+  const oauthGoogle = useCallback(() => {
+    navigate('/oauth2/authorization/google');
+  }, []);
+  // if (isLoading) return <Loading />;
+
+  // if (error) return 'An error has occurred: ';
+  return (
+    <div>
+      <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={handleSubmit(onValid)}>
+        <input
+          {...register('email', {
+            required: 'Email is required',
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9].com$/,
+              message: '이메일 형식으로 입력해주세요.',
+            },
+          })}
+          placeholder="email"
+        />
+        <span>{errors?.email?.message}</span>
+        <input {...register('password', { required: '비빌번호를 입력해주세요.' })} placeholder="password" />
+        <span>{errors?.password?.message}</span>
+        <button>로그인</button>
+        <span>{errors?.extraError?.message}</span>
+      </form>
+      <button onClick={oauthGithub}>깃허브</button>
+      <button onClick={oauthGoogle}>구글</button>
+    </div>
+  );
 }
 
 export default Login;
