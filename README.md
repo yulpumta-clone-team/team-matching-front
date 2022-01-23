e41f2ad0dc7637654214f3a2ab88dfae740f6c06
YUNHO <kimyouknow@naver.com>
2022-01-23 15:36:52 +0900

    Delete all

--- README.md
+++
@@ -1,3 +0,0 @@
-git submodule update --remote
-./gradlew build
-java -jar demo-0.0.1-SNAPSHOT.jar
--- src/typings/userValidation.ts
+++
@@ -1,8 +0,0 @@
-export interface UserValidation {
-  email: string;
-  name: string;
-  nickname: string;
-  password: string;
-  verifiedPassword: string;
-  extraError?: string;
-}
