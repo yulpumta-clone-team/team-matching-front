e5ed1af6e8838eaceb318e751439f77802152232
yunho <kimyouknow@naver.com>
2022-02-20 21:18:59 +0900

    Design: teamboard페이지 그리드를 활용한 스켈레톤 디자인

--- src/components/TeamCard/index.jsx
+++ src/components/TeamCard/index.jsx
@@ -3,13 +3,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes, { objectOf } from 'prop-types';
 import { TEAM_BOARD } from 'utils/route';
-import { CardWrapper } from './style';
+import { CardTitle, CardWrapper, ImgContainer, SessionContainer } from './style';
 
 function TeamCard({ teamInfo }) {
   const { user_id, team_id, name, session, img, read, comment_cnt, like_cnt } = teamInfo;
   return (
-    <CardWrapper key={team_id}>
+    <CardWrapper>
       <Link to={`${TEAM_BOARD}/${team_id}`}>{name}</Link>
+      <CardTitle>{name}</CardTitle>
+      <ImgContainer>
+        <img src={img} alt="임시" />
+      </ImgContainer>
+      <SessionContainer>{session}</SessionContainer>
+      <span>{read}</span>
+      <span>{comment_cnt}</span>
+      <span>{like_cnt}</span>
     </CardWrapper>
   );
 }
--- src/pages/TeamBoard/index.jsx
+++ src/pages/TeamBoard/index.jsx
@@ -3,6 +3,7 @@ import TeamCard from 'components/TeamCard';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getTeamArr } from '_actions/team_action';
+import { BoardWrapper } from './style';
 
 function TeamBoard() {
   const dispatch = useDispatch();
@@ -12,13 +13,12 @@ function TeamBoard() {
   }, []);
   console.log(teamArray);
   return (
-    <ul>
+    <BoardWrapper>
       {teamArray.length !== 0 &&
-        teamArray.map((teamElement) => {
-          console.log(teamElement);
-          return <TeamCard teamInfo={teamElement} />;
-        })}
-    </ul>
+        teamArray.map((teamElement) => (
+          <TeamCard key={teamElement.team_id} teamInfo={teamElement} />
+        ))}
+    </BoardWrapper>
   );
 }
 
