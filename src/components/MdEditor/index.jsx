d997b5d22a54b778c3ef51189ef96b0796747a2b
unknown <hoeun0723@naver.com>
2022-03-01 19:26:32 +0900

    Feat: Teampost입니다

--- src/components/MdEditor/index.jsx
+++ src/components/MdEditor/index.jsx
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor, Viewer } from '@toast-ui/react-editor';
-// import { Box } from './stylemd';
+import { Box } from './stylemd';
 
 function MarkdownEditor({ mdValue }) {
   console.log(mdValue);
