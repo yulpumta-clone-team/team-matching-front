98e9125f5befc107500d9645a1d4f626fc601d6c
unknown <hoeun0723@naver.com>
2022-03-18 19:13:16 +0900

    userslist 진행중

--- src/pages/UsersList/index.jsx
+++ src/pages/UsersList/index.jsx
@@ -1,7 +1,11 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/prop-types */
 /* eslint-disable camelcase */
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import TeamCard from 'components/TeamCard';
 
 const teamElement = {
   user_id: 123123123,
@@ -15,15 +19,18 @@ const teamElement = {
   allArticles: [],
   currentCategory: '읽은목록',
   currentArticles: [],
+  currentTeam: 1,
 
   team: [
     {
+      idx: 1,
       team_id: 1,
-      article: 'team1 소개글',
+      name: 'team1 소개글',
     },
     {
+      idx: 2,
       team_id: 2,
-      article: 'team2 소개글',
+      name: 'team2 소개글',
     },
   ],
 
@@ -38,61 +45,66 @@ const teamElement = {
     },
   ],
 };
-function Tabs() {
-  const { categories } = teamElement;
+function Tabs({ setTeam, setType, categories }) {
+  // const { categories } = teamElement;
+  function tempfunction(event, name, team) {
+    if (name === '읽은목록') {
+      setType('read');
+    } else if (name === '좋아하는 목록') {
+      setType('like');
+    }
+  }
+
+  // console.log(categories);
   return (
     <ul>
-      <li key={categories.id}>
-        <button onClick={(event) => useChangecurrentcategory(event, categories.name)}>{categories.name}</button>
-      </li>
+      {categories.map((category, idx) => (
+        <li key={idx}>
+          <button onClick={(event) => tempfunction(event, category.name)}>{category.name}</button>
+        </li>
+      ))}
     </ul>
   );
 }
-function Articles() {
-  const { categories, allArticles, currentArticles } = teamElement;
+function Articles({ setArticles, type, currentArticles, team }) {
+  const { teamArray } = useSelector((state) => state.team);
+  const [teamList, setTeamList] = useState(teamArray);
+  const { team_id, name, idx } = teamInfo;
+  const { allArticles } = teamElement;
+  const category = this.categories.find(
+    category => category.id === team.team_id
+  );
+  setArticles(team.name);
 
   return (
-    <div key={categories.id}>
-      <small>team #{categories.id}</small>
-      <div>{allArticles}</div>
-    </div>
+    <ul>
+      {type === 'read' ? (
+        <li>
+        {teamList.length !== 0 &&
+          teamList.map((teamElement, idx) => (
+            <TeamCard key={uuid()} teamInfo={{ ...teamElement, idx }} />
+          ))}
+        </li>
+      ) : (
+        <li>???</li>
+      )}
+    </ul>
   );
 }
 
-function useChangecurrentcategory(name) {
-  const { categories, team } = teamElement;
-  const { allArticles } = teamElement;
-  const [currentCategory, setCategory] = useState(teamElement);
-  const [currentArticles, setArticle] = useState(teamElement);
-  // currentCategory = categories.name;
-  // currentArticles = team.article;
-  setCategory(categories.name);
-  setArticle(team.name);
-}
-
 function UsersList() {
-  // const [state, setState] = useState(state);
-  const [read, setRead] = useState(false);
-  const [like_cnt, setLikecnt] = useState(false);
-  const { currentArticles, currentCategory } = teamElement;
-  const { categories } = teamElement;
-
-  const onRead = (e) => {
-    setRead(true);
-    console(read);
-  };
-  const onLike = (e) => {
-    setLikecnt(true);
-    console(like_cnt);
-  };
+  const [type, setType] = useState('read');
+  // const [read, setRead] = useState(false);
+  // const [like_cnt, setLikecnt] = useState(false);
+  const [currentArticles, setArticles] = useState('읽은목록');
+  // const { currentArticles, currentCategory } = teamElement;
+  const { categories, articles } = teamElement;
+  const [currentTeam, setTeam] = useState(1);
+  console.log(type);
   return (
     <div className="App">
-      {/* <Tabs
-        categories={categories}
-        currentCategory={currentCategory}
-        useChangeCurrentCategory={useChangecurrentcategory}
-      />
-      <Articles allArticles={currentArticles} /> */}
+      <Tabs setType={setType} categories={categories} />
+      <Articles type={type} allArticles={currentArticles} setArticles={setArticles} />
     </div>
   );
 }
